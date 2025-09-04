f07da195cbc48e2f9114bed6e458260eb062a860
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-09-04 20:50:36 +0530

    Updated Projects section and button spacing

---
+++ src/types/react-chrono.d.ts
@@ -0,0 +1,44 @@
+declare module "react-chrono" {
+  import * as React from "react";
+
+  export type ChronoMode = "VERTICAL" | "VERTICAL_ALTERNATING" | "HORIZONTAL";
+
+  export interface ChronoItem {
+    title?: string;
+    cardTitle?: string;
+    cardSubtitle?: string;
+    cardDetailedText?: string;
+    [key: string]: any;
+  }
+
+  export interface ChronoProps {
+    items: ChronoItem[];
+    mode?: ChronoMode;
+    slideShow?: boolean;
+    hideControls?: boolean;
+    disableClickOnCircle?: boolean;
+    disableToolbar?: boolean;
+    allowDynamicUpdate?: boolean;
+    cardHeight?: number | string;
+    theme?: {
+      primary?: string;
+      secondary?: string;
+      titleColor?: string;
+      titleColorActive?: string;
+      cardBgColor?: string;
+      cardForeColor?: string;
+      [key: string]: any;
+    };
+    classNames?: {
+      card?: string;
+      title?: string;
+      cardText?: string;
+      [key: string]: any;
+    };
+    style?: React.CSSProperties;
+    onItemSelected?: (index: number) => void;
+    [key: string]: any;
+  }
+
+  export class Chrono extends React.Component<ChronoProps> {}
+}
